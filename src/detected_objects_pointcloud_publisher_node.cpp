d84a056523f5ab982a964d8977cd03620c5f148a
Alexey Panferov <lexavtanke@gmail.com>
2023-03-10 00:10:42 +0800

    wip

--- src/detected_objects_pointcloud_publisher_node.cpp
+++ src/detected_objects_pointcloud_publisher_node.cpp
@@ -1,18 +1,47 @@
 
 
-#include "detected_objects_pointcloud_publisher/detected_objects_pointcloud_publisher.hpp"
+#include "detected_objects_pointcloud_publisher/detected_objects_pointcloud_publisher_node.hpp"
 
-#include <pcl/kdtree/kdtree.h>
+#include <rclcpp/rclcpp.hpp>
 
-int main(int argc, char ** argv)
+namespace detected_objects_pointcloud_publisher 
 {
-  (void) argc;
-  (void) argv;
+  void DetectedObjectsPointcloudPublisher::objectsCallback(const autoware_auto_perception_msgs::msg::DetectedObjects::ConstSharedPtr input_objs_msg)
+  {
 
-  printf("detected_objects_pointcloud_publisher package\n");
+    // Transform to pointcloud frame
+    autoware_auto_perception_msgs::msg::DetectedObjects transformed_objects;
+    if (!transformObjects(
+          *input_objs_msg, pointcloud_frame_id_, tf_buffer_,
+          transformed_objects)) {
+      // objects_pub_->publish(*input_objects);
+      return;
+    }
+    
+    objects_frame_id_ = transformed_objects.header.frame_id;
 
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DetectedObjectsPointcloudPublisher>());
-  rclcpp::shutdown();
-  return 0;
-}
+    objs_buffer.clear();
+    for (const auto & object : transformed_objects.objects)
+    {
+      std::vector<autoware_auto_perception_msgs::msg::ObjectClassification> labels = object.classification;
+      object_info info = {object.shape, object.kinematics.pose_with_covariance.pose, object.classification};
+      objs_buffer.push_back(info);
+    }
+    // RCLCPP_INFO(this->get_logger(), "Update objects buffer");
+  }
+
+
+  int main(int argc, char ** argv)
+  {
+    (void) argc;
+    (void) argv;
+
+    printf("detected_objects_pointcloud_publisher package\n");
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DetectedObjectsPointcloudPublisher>());
+    rclcpp::shutdown();
+    return 0;
+  }
+
+} // namespace detected_objects_pointcloud_publisher
